f7e939774bf89086354689e0e3262950185ba96f
Alex-Welsh <alex@stackhpc.com>
2023-01-19 13:04:28 +0000

    updating the branch name in deploy.sh

--- etc/kayobe/environments/training/deploy.sh
+++ etc/kayobe/environments/training/deploy.sh
@@ -7,7 +7,7 @@ set -eu
 # These parameters must be changed to your own fork 
 
 KAYOBE_CONFIG_REPO="https://github.com/Alex-Welsh/stackhpc-kayobe-config"
-KAYOBE_CONFIG_BRANCH=training-monitoring
+KAYOBE_CONFIG_BRANCH=training-networking
 
 ###################################################
 
